57af47fb5ba9dd4d60667259012966bcb379e941
rishabhkr-r111 <rishabhkrr111@gmail.com>
2024-02-29 02:59:13 +0530

    fix deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/video-chat-app/',
+  base: "/video-chat-app/",
   plugins: [react()],
-})
+
+  build: {
+    rollupOptions: {
+      external: ["react-player"],
+    },
+  },
+});
